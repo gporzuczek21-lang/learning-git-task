72053c2cb294320e4e1d6219f96361f8dc86e438
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-08-14 11:54:21 +0200

    counting products

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ for key, values in shopping_dict.items():
     key_1=key.capitalize()
     new_values=[value.capitalize() for value in values ]
     print(f"ide so {key_1} i kupuje tam{new_values}")
+    s = len(new_values)
+i = s+i
+print(f"W sumie kupuje {i} produktów.")
