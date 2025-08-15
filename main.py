f177edadad2135377817821f84bafa3eae06c3ae
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-08-15 14:59:16 +0200

    mother-in-law

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ for key, values in shopping_dict.items():
 i = s+i
 print(f"W sumie kupuje {i} produktów.")
 print("i 2 kilometry z buta do domu")
-print("zona dzwoni ze trzeba jeszcze mleko")
+print("zona dzwoni ze trzeba jeszcze mleko, bo tesciowa z Maciejem przychodzi na kawe.")
